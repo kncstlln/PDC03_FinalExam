2ce381a4a922f5d3c67cda7923bba4208d673ed6
Kane Castillano <castillano.kaneerryl@auf.edu.ph>
2023-06-10 22:44:10 +0800

    first commit

---
+++ FinalExam/FinalExam/View/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using FinalExam.ViewModel;
+using FinalExam.Model;
+
+namespace FinalExam.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private AnimalViewModel viewModel;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            viewModel = new AnimalViewModel();
+            BindingContext = viewModel;
+        }
+    }
+}
